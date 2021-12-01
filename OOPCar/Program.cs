86af8515013a8f9199a5649b6410e3a0e1bdd8be
Opilane TTHK <opilane@tthk.ee>
2021-12-01 10:25:54 +0200

    loh

---
+++ OOPCar/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace OOPCar
+{
+    class Car
+    {
+        string mark;
+        string model;
+        string regnumber;
+        string color;
+        string odo;
+        string fueltank;
+
+        public Car(string _mark, string _model, string _regnumber, string _color )
+        {
+            mark = _mark;
+            model = _regnumber;
+            color = _color;
+            regnumber = _regnumber;
+            odo = 0;
+            fueltank = 60;
+
+            Console.WriteLine($"{mark} {model} {regnumber} has been created");
+        }
+
+
+    }
+        static void Main(string[] args)
+        {
+        Car myCar = new Car("Bmw", "E39 M3", "P0LEKSN");
+        }
+    }
+}
